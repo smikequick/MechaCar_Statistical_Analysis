2c7820adf91d2541aa0b9eecb7136d7506b6549d
smikequick <87885677+smikequick@users.noreply.github.com>
2021-12-22 09:03:10 -0700

    Adding summary statistics per lot for coil data

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+#Deliverable 1
 > library(dplyr)
 
 Attaching package: ‘dplyr’
@@ -48,3 +49,29 @@ Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’
 Residual standard error: 1.15 on 44 degrees of freedom
 Multiple R-squared:  0.7119,	Adjusted R-squared:  0.6791 
 F-statistic: 21.74 on 5 and 44 DF,  p-value: 6.712e-11
+
+#Deliverable 2
+
+> suspension <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+> View(suspension)
+> head(suspension)
+  VehicleID Manufacturing_Lot  PSI
+1    V40858              Lot1 1499
+2    V40607              Lot1 1500
+3    V31443              Lot1 1500
+4     V6004              Lot1 1500
+5     V7000              Lot1 1501
+6    V17344              Lot1 1501
+> Mean = mean(suspension$PSI)
+> Median = median (suspension$PSI)
+> Variance = var(suspension$PSI)
+> SD = sd(suspension$PSI)
+
+> #Total Summary Data Frame
+> total_sum <-data.frame(Mean, Median, Variance, SD)
+> View(total_sum)
+
+> # Lot Summary
+> lot_sum <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+> View(lot_sum)
+>
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # MechaCar_Statistical_Analysis
+
+## Linear Regression to Predict MPG
+
+*Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
+
+Vehicle weight (-0.0002) and spoiler angle (-0.01) provide a non-random amount of variance to the mpg values in the dataset.
+
+*Is the slope of the linear model considered to be zero? Why or why not?
+
+The slope of the linear model is not considered zero since the p-value is 0.00000000006712. Comparing this to the significance level, we can reject a null hypothesis.
+
+
+*Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
+
+Yes, this linear model predicts mpg of MechaCar prototypes effectively. The multiple R-squared value is equal to 0.7119 or 71.19% which means that ~71% of the time the predictions will be accurate using this linear model.
+
