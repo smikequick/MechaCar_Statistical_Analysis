8d67bbef99614226820ed20c5c481e091dcdff1f
smikequick <87885677+smikequick@users.noreply.github.com>
2021-12-22 09:17:46 -0700

    Adding t-test analysis by lot and PSI

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -75,3 +75,57 @@ F-statistic: 21.74 on 5 and 44 DF,  p-value: 6.712e-11
 > lot_sum <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 > View(lot_sum)
 >
+
+# Deliverable 3
+
+t.test((suspension$PSI), mu=1500)
+
+    One Sample t-test
+
+data:  (suspension$PSI)
+t = -1.8931, df = 149, p-value = 0.06028
+alternative hypothesis: true mean is not equal to 1500
+95 percent confidence interval:
+ 1497.507 1500.053
+sample estimates:
+mean of x
+  1498.78
+
+> t.test(subset(suspension,Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+
+    One Sample t-test
+
+data:  subset(suspension, Manufacturing_Lot == "Lot1")$PSI
+t = 0, df = 49, p-value = 1
+alternative hypothesis: true mean is not equal to 1500
+95 percent confidence interval:
+ 1499.719 1500.281
+sample estimates:
+mean of x
+     1500
+
+> t.test(subset(suspension, Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
+
+    One Sample t-test
+
+data:  subset(suspension, Manufacturing_Lot == "Lot2")$PSI
+t = 0.51745, df = 49, p-value = 0.6072
+alternative hypothesis: true mean is not equal to 1500
+95 percent confidence interval:
+ 1499.423 1500.977
+sample estimates:
+mean of x
+   1500.2
+
+> t.test(subset(suspension, Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
+
+    One Sample t-test
+
+data:  subset(suspension, Manufacturing_Lot == "Lot3")$PSI
+t = -2.0916, df = 49, p-value = 0.04168
+alternative hypothesis: true mean is not equal to 1500
+95 percent confidence interval:
+ 1492.431 1499.849
+sample estimates:
+mean of x
+  1496.14 
--- README.md
+++ README.md
@@ -15,3 +15,9 @@ The slope of the linear model is not considered zero since the p-value is 0.0000
 
 Yes, this linear model predicts mpg of MechaCar prototypes effectively. The multiple R-squared value is equal to 0.7119 or 71.19% which means that ~71% of the time the predictions will be accurate using this linear model.
 
+## Summary Statistics on Suspension Coils
+
+*The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch. Does the current manufacturing data meet this design specification for all manufacturing lots in total and each lot individually? Why or why not?
+
+Overall, the lots in total do meet this design specification with a variance of 62.29356, which does not exceed the 100 pounds per square inch requirement. However, when diving deeper into individual lots, Lot 1 and Lot 2 meet the requirement with 0.98 and 7.47 respectively, while Lot 3 has a variance of 170.29. Therefore, Lots 1 and 2 meet the requirement while Lot 3 does not.
+
